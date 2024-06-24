51a48ca2e1795f6d53e0a26169e8fc5b848f1ae8
Robert Heller <heller@deepsoft.com>
2024-06-24 10:14:39 -0400

    Start on cut diagram for headlight switch.

--- HeadlightSwitch/BodyCoverDrillSheet.py
+++ HeadlightSwitch/BodyCoverDrillSheet.py
@@ -8,7 +8,7 @@
 #  Author        : $Author$
 #  Created By    : Robert Heller
 #  Created       : Sun Jun 23 19:39:24 2024
-#  Last Modified : <240624.0006>
+#  Last Modified : <240624.1013>
 #
 #  Description	
 #
@@ -50,6 +50,8 @@ import os
 import sys
 sys.path.append(os.path.dirname(__file__))
 
+import datetime
+
 class HeadlightSwitchBodyCoverDrillSheet(object):
     __BoardWidth  = 88.35
     __BoardHeight = 25.4
@@ -76,7 +78,7 @@ class HeadlightSwitchBodyCoverDrillSheet(object):
         self.origin = origin
         cover = Part.makePlane(self.__BoardWidth,self.__BoardHeight,origin)\
                 .extrude(Base.Vector(0,0,self.__CoverThick))
-        switchhole = Part.Face(Part.Wire(Part.makeCircle(self.__SwitchHoleR,origin.add(Base.Vector(self.__SwitchHoleX,self.__SwitchHoleY,0)))))\
+        switchhole = Part.Face(Part.Wire(Part.makeCircle(self.__SwitchHoleR,origin.add(Base.Vector(self.__SwitchHoleX,self.__BoardHeight-self.__SwitchHoleY,0)))))\
                 .extrude(Base.Vector(0,0,self.__CoverThick))
         cover = cover.cut(switchhole)
         mounthole = Part.Face(Part.Wire(Part.makeCircle(self.__MountHoleR,origin.add(Base.Vector(self.__MountHoleX1,self.__MountHoleY,0)))))\
@@ -85,21 +87,21 @@ class HeadlightSwitchBodyCoverDrillSheet(object):
         mounthole = Part.Face(Part.Wire(Part.makeCircle(self.__MountHoleR,origin.add(Base.Vector(self.__MountHoleX2,self.__MountHoleY,0)))))\
                 .extrude(Base.Vector(0,0,self.__CoverThick))
         cover = cover.cut(mounthole)
-        LEDHole = Part.Face(Part.Wire(Part.makeCircle(self.__LEDsR,origin.add(Base.Vector(self.__LEDHiBeamX,self.__LEDsY,0)))))\
+        LEDHole = Part.Face(Part.Wire(Part.makeCircle(self.__LEDsR,origin.add(Base.Vector(self.__LEDHiBeamX,self.__BoardHeight-self.__LEDsY,0)))))\
                 .extrude(Base.Vector(0,0,self.__CoverThick))
         cover = cover.cut(LEDHole)
-        LEDHole = Part.Face(Part.Wire(Part.makeCircle(self.__LEDsR,origin.add(Base.Vector(self.__LEDLoBeamX,self.__LEDsY,0)))))\
+        LEDHole = Part.Face(Part.Wire(Part.makeCircle(self.__LEDsR,origin.add(Base.Vector(self.__LEDLoBeamX,self.__BoardHeight-self.__LEDsY,0)))))\
                 .extrude(Base.Vector(0,0,self.__CoverThick))
         cover = cover.cut(LEDHole)
-        LEDHole = Part.Face(Part.Wire(Part.makeCircle(self.__LEDsR,origin.add(Base.Vector(self.__LEDMarkerX,self.__LEDsY,0)))))\
+        LEDHole = Part.Face(Part.Wire(Part.makeCircle(self.__LEDsR,origin.add(Base.Vector(self.__LEDMarkerX,self.__BoardHeight-self.__LEDsY,0)))))\
                 .extrude(Base.Vector(0,0,self.__CoverThick))
         cover = cover.cut(LEDHole)
         self.cover = cover
-        self.rodHiBeam = Part.Face(Part.Wire(Part.makeCircle(self.__LEDsR,origin.add(Base.Vector(self.__LEDHiBeamX,self.__LEDsY,self.__CoverThick)))))\
+        self.rodHiBeam = Part.Face(Part.Wire(Part.makeCircle(self.__LEDsR,origin.add(Base.Vector(self.__LEDHiBeamX,self.__BoardHeight-self.__LEDsY,self.__CoverThick)))))\
                 .extrude(Base.Vector(0,0,-self.__LEDRodLength))
-        self.rodLoBeam = Part.Face(Part.Wire(Part.makeCircle(self.__LEDsR,origin.add(Base.Vector(self.__LEDLoBeamX,self.__LEDsY,self.__CoverThick)))))\
+        self.rodLoBeam = Part.Face(Part.Wire(Part.makeCircle(self.__LEDsR,origin.add(Base.Vector(self.__LEDLoBeamX,self.__BoardHeight-self.__LEDsY,self.__CoverThick)))))\
                 .extrude(Base.Vector(0,0,-self.__LEDRodLength))
-        self.rodMarker = Part.Face(Part.Wire(Part.makeCircle(self.__LEDsR,origin.add(Base.Vector(self.__LEDMarkerX,self.__LEDsY,self.__CoverThick)))))\
+        self.rodMarker = Part.Face(Part.Wire(Part.makeCircle(self.__LEDsR,origin.add(Base.Vector(self.__LEDMarkerX,self.__BoardHeight-self.__LEDsY,self.__CoverThick)))))\
                 .extrude(Base.Vector(0,0,-self.__LEDRodLength))
         standOff = Part.Face(Part.Wire(Part.makeCircle(self.__MountStandR,origin.add(Base.Vector(self.__MountHoleX1,self.__MountHoleY,0)))))\
                 .extrude(Base.Vector(0,0,-self.__SwitchDepth))
@@ -116,6 +118,7 @@ class HeadlightSwitchBodyCoverDrillSheet(object):
         obj.Shape=self.cover
         obj.Label=self.name+"_cover"
         obj.ViewObject.ShapeColor=tuple([0.7,0.7,0.7])
+        self.coverObj = obj
         obj = doc.addObject("Part::Feature",self.name+"_HiBeam")
         obj.Shape=self.rodHiBeam
         obj.Label=self.name+"_HiBeam"
@@ -141,12 +144,107 @@ class HeadlightSwitchBodyCoverDrillSheet(object):
         obj.Label=self.name+"_standOff2"
         obj.ViewObject.ShapeColor=tuple([0.7,0.7,0.7])
         obj.ViewObject.Transparency=50
-
+    def TechPage(self,doc=None):
+        if doc==None:
+            doc = App.activeDocument()
+        for g in doc.findObjects('TechDraw::DrawSVGTemplate'):
+            doc.removeObject(g.Name)
+        for g in doc.findObjects('TechDraw::DrawPage'):
+            doc.removeObject(g.Name)
+        for g in doc.findObjects('Spreadsheet::Sheet'):
+            doc.removeObject(g.Name)
+        for g in doc.findObjects('TechDraw::DrawViewPart'):
+            doc.removeObject(g.Name)
+        for g in doc.findObjects('TechDraw::DrawViewDimension'):
+            doc.removeObject(g.Name)
+        doc.addObject('TechDraw::DrawSVGTemplate','USLetterTemplate')
+        doc.USLetterTemplate.Template = App.getResourceDir()+"Mod/TechDraw/Templates/USLetter_Landscape.svg"
+        edt = doc.USLetterTemplate.EditableTexts
+        doc.addObject('TechDraw::DrawSVGTemplate','USLetterTemplate')
+        doc.USLetterTemplate.Template = App.getResourceDir()+"Mod/TechDraw/Templates/USLetter_Landscape.svg"
+        edt = doc.USLetterTemplate.EditableTexts
+        edt['CompanyName'] = "Deepwoods Software"
+        edt['CompanyAddress'] = '51 Locke Hill Road, Wendell, MA 01379 USA'    
+        edt['DrawingTitle1']= 'Headlight Switch Body Cover'
+        edt['DrawingTitle3']= ""
+        edt['DrawnBy'] = "Robert Heller"
+        edt['CheckedBy'] = ""
+        edt['Approved1'] = ""
+        edt['Approved2'] = ""
+        edt['Code'] = ""
+        edt['Weight'] = ''
+        edt['DrawingNumber'] = datetime.datetime.now().ctime()
+        edt['Revision'] = "A"
+        doc.USLetterTemplate.EditableTexts = edt
+        doc.addObject('TechDraw::DrawPage','BodyCoverDrillSheet')        
+        doc.BodyCoverDrillSheet.Template = doc.USLetterTemplate
+        edt = doc.BodyCoverDrillSheet.Template.EditableTexts
+        edt['DrawingTitle2']= "Drill Template"
+        edt['Scale'] = '1.0'
+        edt['Sheet'] = "Sheet 1 of 1"
+        doc.BodyCoverDrillSheet.Template.EditableTexts = edt
+        doc.BodyCoverDrillSheet.ViewObject.show() 
+        drillsheet = doc.addObject('Spreadsheet::Sheet','DrillTemplateDimensionTable')
+        drillsheet.set("A1",'%-11.11s'%"Dim")
+        drillsheet.set("B1",'%10.10s'%"inch")
+        drillsheet.set("C1",'%10.10s'%"mm") 
+        ir = 2
+        doc.addObject('TechDraw::DrawViewPart','CoverTopView')
+        doc.BodyCoverDrillSheet.addView(doc.CoverTopView)
+        doc.CoverTopView.Source = self.coverObj
+        doc.CoverTopView.Direction=(0.0,0.0,1.0)
+        doc.CoverTopView.Scale = 1
+        doc.CoverTopView.X = 80
+        doc.CoverTopView.Y = 130
+        minX = 999999999
+        minY = 999999999
+        maxX = 0
+        maxY = 0
+        i = 0
+        for v in self.cover.Vertexes:
+           if v.X < minX:
+               minX = v.X
+           if v.X > maxX:
+               maxX = v.X    
+           if v.Y < minY:
+               minY = v.Y
+           if v.Y > maxY:
+               maxY = v.Y
+           print ('*** TechPage(): Vertexes[%d] at (%g,%g,%g)'%(i,v.X,v.Y,v.Z))
+           i += 1
+        length = maxX - minX
+        height = maxY - minY
+        print ('*** TechPage(): origin (%g,%g), length = %g, height = %g'%(minX,minY,length,height))    
+        i = 0
+        for e in self.cover.Edges:
+            if isinstance(e.Curve,Part.Circle):
+                #print('*** TechPage(): Edges[%d].Curve is a %s'%(i,type(e.Curve)))
+                circ = e.Curve
+                print('*** TechPage(): Edges[%d].Curve %g at (%g,%g,%g)'%\
+                      (i,circ.Radius*2,circ.Location.x,circ.Location.y,circ.Location.z))
+            i += 1
+        doc.addObject('TechDraw::DrawViewDimension','SwitchShaftHDia')
+        doc.SwitchShaftHDia.Type = 'Diameter'
+        doc.SwitchShaftHDia.References2D=[(doc.CoverTopView,'Edge5')]
+        # 10:11:43  IndexError: {'sclassname': 'N4Base10IndexErrorE', 'sErrMsg': 'DVP::getEdge - No edges found.', 'sfile': '', 'iline': 0, 'sfunction': '', 'swhat': 'DVP::getEdge - No edges found.', 'btranslatable': False, 'breported': False}
+        doc.SwitchShaftHDia.FormatSpec='SWHDia'
+        doc.SwitchShaftHDia.Arbitrary = True
+        doc.SwitchShaftHDia.X = -30
+        doc.SwitchShaftHDia.Y = 10
+        doc.BodyCoverDrillSheet.addView(doc.SwitchShaftHDia)
+        drillsheet.set("A%d"%ir,'%-11.11s'%"SWHDia")
+        drillsheet.set("B%d"%ir,'%10.6f'%((self.__SwitchHoleR*2)/25.4))
+        drillsheet.set("C%d"%ir,'%10.6f'%(self.__SwitchHoleR*2))
+        doc.recompute()
+        
 if __name__ == '__main__':
+    docs = App.listDocuments()
+    for k in docs:
+        App.closeDocument(k)
     App.ActiveDocument=App.newDocument("Temp")
     doc = App.activeDocument()
     cover = HeadlightSwitchBodyCoverDrillSheet("cover",Base.Vector(0,0,0))
     cover.show()
     Gui.SendMsgToActiveView("ViewFit")
     Gui.activeDocument().activeView().viewTop()
-    
+    cover.TechPage()
